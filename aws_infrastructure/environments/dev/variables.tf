9a512562daa4a30afb5db9b9c15e3e03262191a2
eliezer ibghi <iibgui@yahoo.fr>
2025-05-22 15:33:12 +0200

    test

--- aws_infrastructure/environments/dev/variables.tf
+++ aws_infrastructure/environments/dev/variables.tf
@@ -19,13 +19,13 @@ variable "subnet_cidr" {
 variable "ami_id" {
   description = "AMI ID for EC2 instances"
   type        = string
-  default     = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI (us-east-1)
+  default     = "ami-0953476d60561c955" # Amazon Linux 2 AMI (us-east-1)
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t2.micro"
+  default     = "t2.smal"
 }
 
 variable "instance_count" {
