c04ea710ea2c4f98c7f95e9a518ce7213b358f30
eliezer ibghi <iibgui@yahoo.fr>
2025-05-28 17:56:12 +0200

    modification du path dans le fichier argo

--- aws_infrastructure/modules/ec2/main.tf
+++ aws_infrastructure/modules/ec2/main.tf
@@ -9,10 +9,10 @@ resource "aws_instance" "web" {
   user_data = <<-EOF
               #!/bin/bash
               yum update -y
-              yum install -y httpd
-              systemctl start httpd
-              systemctl enable httpd
-              echo "<h1>Hello from ${var.environment} EC2</h1>" > /var/www/html/index.html
+              yum install -y nginx
+              systemctl start nginx
+              systemctl enable nginx
+              echo "<h1>Hello from ${var.environment} EC2</h1>" > /usr/share/nginx/html/index.html
               EOF
 
   tags = {
