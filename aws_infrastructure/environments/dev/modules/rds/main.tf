a94811ef71f5532ea2d9a1adaf8d5a5b4bb98313
eliezer ibghi <iibgui@yahoo.fr>
2025-06-08 21:17:43 +0200

    add action-terraform

--- aws_infrastructure/environments/dev/modules/rds/main.tf
+++ aws_infrastructure/environments/dev/modules/rds/main.tf
@@ -1,17 +1,17 @@
 resource "aws_db_instance" "rds" {
   identifier           = "${var.environment}-rds-v3" # Nom unique pour éviter le conflit
-  allocated_storage    = 20
-  storage_type        = "gp2"
-  engine              = "mysql"
-  engine_version      = "8.0"
-  instance_class      = var.db_instance_class
-  db_name             = var.db_name
-  username            = "admin"
+  allocated_storage    = 20 # Taille du stockage en Go
+  storage_type        = "gp2" # Type de stockage (gp2 pour SSD général)
+  engine              = "mysql" # Type de base de données (MySQL)
+  engine_version      = "8.0" # Version de MySQL à utiliser
+  instance_class      = var.db_instance_class # Classe de l'instance (e.g., db.t3.micro)
+  db_name             = var.db_name # Nom de la base de données à créer
+  username            = "admin" # Nom d'utilisateur pour la base de données
   password            = "SecurePassword123!" # À remplacer par un secret manager en production
-  vpc_security_group_ids = [var.security_group_id]
-  db_subnet_group_name = aws_db_subnet_group.rds.name
-  skip_final_snapshot  = true
-  multi_az             = false
+  vpc_security_group_ids = [var.security_group_id] # ID du groupe de sécurité pour la base de données
+  db_subnet_group_name = aws_db_subnet_group.rds.name # Nom du groupe de sous-réseaux pour la base de données
+  skip_final_snapshot  = true # Ne pas prendre de snapshot final lors de la suppression de l'instance
+  multi_az             = false # Ne pas activer Multi-AZ pour la haute disponibilité
 
   tags = {
     Name        = "${var.environment}-rds-v3"
