503ea4d043e43b183cb30d9dc582898225e09152
eliezer ibghi <iibgui@yahoo.fr>
2025-06-11 12:15:06 +0200

    Add ArgoCD test tag

--- aws_infrastructure/environments/dev/init_instance.tf
+++ aws_infrastructure/environments/dev/init_instance.tf
@@ -44,6 +44,19 @@
 # Runs a script to import db.dump.sql into RDS
 # Instance EC2 pour l'initialisation de la base de données
 # Exécute un script pour importer db.dump.sql dans RDS
+resource "aws_eip" "init_instance_eip" {
+  domain = "vpc"
+  tags = {
+  Name = "init-instance"
+  ArgoCD = "test"
+}
+}
+
+resource "aws_eip_association" "init_instance_eip_assoc" {
+  instance_id   = aws_instance.init_instance.id
+  allocation_id = aws_eip.init_instance_eip.id
+}
+
 resource "aws_instance" "init_instance" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
@@ -56,7 +69,7 @@ resource "aws_instance" "init_instance" {
               #!/bin/bash
               apt-get update
               apt-get install -y mysql-client-core-8.0
-              mysql -h ${aws_db_instance.mysql_rds.endpoint} -u ${var.db_username} -p${var.db_password} ${var.db_name} < /tmp/db.sql
+              mysql -h ${aws_db_instance.mysql_rds.endpoint} -u ${var.db_username} -p${var.db_password} todolist < /tmp/db.sql
               shutdown -h now
               EOF
 
